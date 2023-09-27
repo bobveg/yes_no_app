b5e47bad2c6858ac9013aed277474f4cc065b810
bobveg <60653729+bobveg@users.noreply.github.com>
2023-09-27 02:30:57 +0200

    Campo entrada texto

--- lib/presentation/widgets/chat/shared/message_field_box.dart
+++ lib/presentation/widgets/chat/shared/message_field_box.dart
@@ -5,17 +5,28 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return TextFormField(
-      decoration: InputDecoration(
-        filled: true,
-        suffixIcon: IconButton(
-          icon: const Icon(Icons.send_outlined),
-          onPressed: () {
-            
-          },
-        ),
-        hintText: 'Escribe un mensaje',
-      )
-    );
+        decoration: InputDecoration(
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: colors.primary),
+          borderRadius: BorderRadius.circular(20)),
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: () {},
+      ),
+      hintText: 'Escribe un mensaje',
+    ));
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
